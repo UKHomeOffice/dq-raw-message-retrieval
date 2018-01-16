df2c0de58ad48212ede15d43405d6cf57d1d3d3e
nick-otter <nicholas.a.otter@gmail.com>
2018-01-16 14:54:51 +0000

    s3 update

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
+const AWS = require('aws-sdk')
+const fs = require('fs')
 const http = require('http')
 const admzip = require('adm-zip')
 const readFileAsync = require('util').promisify(require('fs').readFile)
+let s3 = new AWS.S3({
+  params: {Bucket: process.env.Bucket}
+})
 
 const header = '<!DOCTYPE html>\n' +
   '<html>\n' +
@@ -11,28 +16,19 @@ const footer = '</font>\n' +
   '</body>\n' +
   '</html>\n'
 
-const zip_root_path = 'E:\\RAW_ARCHIVE'
+const get_zip_path_from_url = url => /\/\w+$/.exec(url)[0]
 
-const get_zip_path_from_url = url => new Promise((resolve, reject) => {
-  let result = new RegExp('((?<=^\\/.+\\/)[^\\/]+)((?<=_)[^\\/]+)').exec(url)
-  if (!result) {
-    return reject('did not match')
-  }
-  resolve(`${zip_root_path}/${result[2]}/${result[0]}`)
-})
-
-const get_filepath_in_zip_from_url = url => /\w+\/\w+$/.exec(url)[0]
-
-const make_buffer_from_file = file_path =>
-  readFileAsync(file_path)
+const get_zip_name_from_url = url => /\w+$/.exec(url)[0]
 
 const read_file_from_zip_buffer = (buffer, file_path) =>
   new admzip(buffer).readAsText(file_path)
 
-const request_handler = (req, res) =>
-  get_zip_path_from_url(req.url)
-    .then(make_buffer_from_file)
-    .then(buffer => read_file_from_zip_buffer(buffer, get_filepath_in_zip_from_url(req.url)))
+const request_handler = (req, res) => {
+  const zip_path = get_zip_path_from_url(req.url);
+  const zip_name = get_zip_name_from_url(req.url);
+
+  s3.getObject({ Key: zip_name }).promise()
+    .then(response => read_file_from_zip_buffer(response.Body, zip_path))
     .then(payload => {
       res.writeHead(200, {'Content-Type': 'text/html'})
       return res.end(templated_response(payload))
@@ -41,7 +37,10 @@ const request_handler = (req, res) =>
       res.writeHead(404)
       return res.end(e)
     })
+}
 
 const http_server = http.createServer(request_handler).listen(3000)
+module.exports = http_server
+
 
 const templated_response = payload => header + payload + footer
--- app/test/index.test.js
+++ app/test/index.test.js
@@ -2,6 +2,7 @@ const rewire = require('rewire')
 const app = rewire('../')
 const chai = require('chai')
 const sinon = require('sinon')
+const request = require('supertest');
 
 chai.use(require('sinon-chai'))
 
@@ -10,24 +11,17 @@ var expect = chai.expect
 
 describe('RMR tool', () => {
   describe('get_zip_path_from_url', () => {
-    it('should return the correct path from the url', () =>
-      expect(app.__get__('get_zip_path_from_url')('/ignored/zipfile_date/filedir/filename')).to.eventually.equal('E:\\RAW_ARCHIVE/date/zipfile_date'))
-  })
-
-  describe('get_filepath_in_zip_from_url', () => {
-    it('should return the correct path inside the zip from the url', () =>
-      expect(app.__get__('get_filepath_in_zip_from_url')
+    it('should return the correct path for the zip from the url', () =>
+      expect(app.__get__('get_zip_path_from_url')
       ('/ignored/zipfile_date/filedir/filename'))
-        .to.equal('filedir/filename'))
+        .to.equal('/filename'))
   })
 
-  describe('make_buffer_from_file', () => {
-    it('should return a buffer given a file', () =>
-      expect(app.__get__('make_buffer_from_file')('../zip_file_fixture.zip')).to.eventually.be.instanceof(Buffer)
-    )
-    it('should reject an invalid file', () =>
-      expect(app.__get__('make_buffer_from_file')('nofile')).to.be.rejected
-    )
+  describe('get_zip_name_from_url', () => {
+    it('should return the correct zip name from the url', () =>
+      expect(app.__get__('get_zip_name_from_url')
+      ('/ignored/zipfile_date/filedir/filename'))
+        .to.equal('filename'))
   })
 
   describe('read_file_from_zip_buffer', () => {
@@ -48,7 +42,7 @@ describe('RMR tool', () => {
         .then(() =>
           expect(res.writeHead).to.be.calledWith(200, {'Content-Type': 'text/html'}))
     )
-
+    
     it('should return everything we expect', () =>
       app.__get__('request_handler')(req, res)
         .then(() =>
@@ -62,6 +56,17 @@ describe('RMR tool', () => {
         )
     )
   })
+
+  describe('Browser Tests', function() {
+    xit('should display zip contents successfully', function(done) {
+      return request(app)
+        .get('/')
+        .expect(200)
+        .then(response => {
+          assert(response.body, 'foobar')
+        })
+    });
+  });
 
   after(() => {
     app.__get__('http_server').close()
